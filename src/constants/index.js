0e8f4351b957f498ebfa7a2ed4678780fe55627a
Allen Kubai <allen2hola@gmail.com>
2018-12-11 20:14:44 +0300

    fixed issue with api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/'
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
 export const DEFAULT_PAGE_SIZE = 10;
 
